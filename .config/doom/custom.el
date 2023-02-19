e2791d217e0b997363620eb197e62a651b614e84
itsKia2 <omerkarimi29@gmail.com>
2023-02-18 20:37:40 -0500

    emacs: custom var change

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
  '(company-backends '(company-capf))
- '(org-agenda-files '("/home/kia/work.org")))
+ '(org-agenda-files '("~/Documents/work.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
