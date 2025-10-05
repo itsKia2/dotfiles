ca93478d7e96b05a607462a529e8f38226c1025b
itsKia2 <omerkarimi29@gmail.com>
2025-10-04 22:53:32 -0400

    minor editor changes

--- .config/nvim/lua/marshmalon/set.lua
+++ .config/nvim/lua/marshmalon/set.lua
@@ -1,14 +1,14 @@
 vim.cmd.colorscheme("tokyonight")
 
-vim.opt.clipboard = 'unnamedplus' -- use system keyboard for yank
+vim.opt.clipboard = "unnamedplus" -- use system keyboard for yank
 
-vim.opt.nu = true                 -- set line numbers -- set line numbers
-vim.opt.relativenumber = false     -- use relative line numbers
+vim.opt.nu = true -- set line numbers -- set line numbers
+vim.opt.relativenumber = false -- use relative line numbers
 
 -- set tab size to 2 spaces
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
