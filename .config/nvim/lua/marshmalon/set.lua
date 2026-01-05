b9bc124c8c6069851ff27349ba46c6c61ef6462f
itsKia2 <omerkarimi29@gmail.com>
2026-01-05 18:33:29 -0500

    nvim diagnostics

--- .config/nvim/lua/marshmalon/set.lua
+++ .config/nvim/lua/marshmalon/set.lua
@@ -17,3 +17,20 @@ vim.opt.wrap = false
 vim.opt.incsearch = true -- incremental search
 
 vim.opt.termguicolors = true
+
+vim.diagnostic.config({
+  virtual_text = {
+    severity = vim.diagnostic.severity.ERROR,
+    spacing = 4,
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    vim.diagnostic.open_float(nil, { focus = false, scope = "line" })
+  end,
+})
